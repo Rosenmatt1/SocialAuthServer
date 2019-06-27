71256ee3df6438272f31fbc9566ee7ad7998e8ad
Matt Winzer <matt.winzer@gmail.com>
2019-06-27 15:37:02 -0600

    installed dependencies and initialized linter

---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+
+const PORT = process.env.PORT || 4000
+const app = express()
+
+// Middleware
+app.use(cors())
+app.use(morgan('dev'))
+
+// Router files
+
+
+// Routes
+app.get('/api/test', (req, res) => {
+  res.json({
+    message: 'Route working'
+  })
+  // const error = new Error('it blew up')
+  // next(error)
+})
+ 
+// Error handling
+// The following 2 `app.use`'s MUST follow ALL your routes/middleware
+app.use(notFound)
+app.use(errorHandler)
+
+// eslint-disable-next-line
+function notFound(req, res, next) {
+  res.status(404).send({ error: 'Not found!', status: 404, url: req.originalUrl })
+}
+
+// eslint-disable-next-line
+function errorHandler(err, req, res, next) {
+  console.error('ERROR', err)
+  const stack = process.env.NODE_ENV !== 'production' ? err.stack : undefined
+  res.status(500).send({ error: err.message, stack, url: req.originalUrl })
+}
+
+app.listen(PORT, () => {
+  console.log(`Server running on port: ${PORT}`)
+})
+
