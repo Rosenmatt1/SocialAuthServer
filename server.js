7e03743d5b3c9bd26b31f84bc5f3c1b50ab05dfa
Matthew Rosenberg <Rosenmatt1@gmail.com>
2019-07-18 14:59:45 -0600

    Configured protected endpoint with jwt

--- server.js
+++ server.js
@@ -3,9 +3,32 @@ const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
+const jwt = require('express-jwt')
+const jwksRsa = require('jwks-rsa')
 
 const PORT = process.env.PORT || 4000
 const app = express()
+
+// Set up Auth0 configuration
+const authConfig = {
+  domain: 'rosenmatt1.auth0.com',
+  audience: 'http://localhost4000'
+}
+
+// Define middleware that validates incoming bearer tokens
+// using JWKS from YOUR_DOMAIN
+const checkJwt = jwt({
+  secret: jwksRsa.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: `https://${authConfig.domain}/.well-known/jwks.json`
+  }),
+
+  audience: authConfig.audience,
+  issuer: `https://${authConfig.domain}/`,
+  algorithm: ['RS256']
+})
 
 // Middleware
 app.use(cors())
@@ -89,6 +112,14 @@ app.get('/api/products/:id', (req, res, next) => {
       console.log(error)
     })
 })
+
+// Define an endpoint that must be called with an access token
+app.get('/api/external', checkJwt, (req, res) => {
+  res.send({
+    msg: 'Your Access Token was successfully validated!'
+  })
+})
+
  
 // Error handling
 // The following 2 `app.use`'s MUST follow ALL your routes/middleware
